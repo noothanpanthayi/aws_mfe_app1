dff5b0cc61781180b238b7bc29d67d9d1547d723
noothanpanthayi <noothankrishnan@outlook.com>
2024-12-18 15:34:56 -0600

    update1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+// import { useState } from 'react';
 import viteLogo from '/vite.svg';
 import flower from './assets/flower.svg';
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,13 +18,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="card1">
+        <div className="appName1">React Application 1</div>
+        <div>
+        <a target="_blank" href="https://main.d1294rbaaroizz.amplifyapp.com/">Hosted on AWS</a>
+         </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
