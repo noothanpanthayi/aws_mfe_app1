b7bb942f1f2d416eba29c0fc200ee50e864ed06c
noothanpanthayi <noothankrishnan@outlook.com>
2024-12-27 11:53:13 -0600

    app1 update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+  <Routes>
+    <Route path="/child1" element={<App/>}></Route>
+  </Routes>
+ </BrowserRouter>
 )
