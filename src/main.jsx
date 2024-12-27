458aa9eab4168c5e1fca33c6fd420ce5cfaf4c30
noothanpanthayi <noothankrishnan@outlook.com>
2024-12-27 13:45:24 -0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <Routes>
-    <Route path="/child1" element={<App/>}></Route>
-  </Routes>
- </BrowserRouter>
+ <App/>
 )
